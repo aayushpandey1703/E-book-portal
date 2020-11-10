efe10b864b08a1ca5d52b7c5267577df62d8683f
@_1703_ <48384216+aayushpandey1703@users.noreply.github.com>
2020-11-10 14:08:39 +0530

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'connection.php';
-$query="delete from books where bid='".$_POST['bid']."'";
-$result=  mysqli_query($con, $query);
-if($result)
-{
-    header("location:all_books.php");
-}
-?>
